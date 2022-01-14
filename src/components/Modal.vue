91f73f8e9effabb713f6a1346e6cde36ac4d0323
pacblinky <superboychannel321@gmail.com>
2022-01-14 13:05:56 +0200

    some changes

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -4,13 +4,12 @@
     <div class="modal-content bg-dark text-white">
       <div class="modal-header">
         <h5 class="modal-title">{{modal_title}}</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button" class="btn-close" @click="modal.hide()" aria-label="Close"></button>
       </div>
-      <div class="modal-body bg-dark text-white">
+      <div class="modal-body bg-dark">
         <slot :name="body"></slot>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
         <slot :name="footer"></slot>
       </div>
     </div>
